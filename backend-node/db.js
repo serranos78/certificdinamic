66a57cf40b07161aeb08959f08311072e11da401
serranos78 <gerardo07927@gmail.com>
2026-05-14 09:15:50 -0600

    Fixes para Railway p2

--- backend-node/db.js
+++ backend-node/db.js
@@ -9,11 +9,12 @@ const mysql = require('mysql2');
 
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT // ✅ IMPORTANTE
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+ // ✅ IMPORTANTE
 });
 
 
