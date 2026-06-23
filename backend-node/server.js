473f4a5351c79d188fb357890692486c057b51c9
serranos78 <gerardo07927@gmail.com>
2026-06-23 00:08:34 -0600

    Subiendo cambios

--- backend-node/server.js
+++ backend-node/server.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: "https://dinamicpage.netlify.app"
+}));
+
 app.use(express.json());
 
 // ✅ TODAS LAS RUTAS
